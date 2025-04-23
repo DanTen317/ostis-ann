910acaadbb008c16d97f36199f2e2a6ee5d020d2
Астахов Артём <artemastahov27@gmail.com>
2025-04-23 20:16:35 +0300

    fix: port on the fronend changed by 9000

--- problem-solver/py/app/frontend/src/library/utils.ts
+++ problem-solver/py/app/frontend/src/library/utils.ts
@@ -38,7 +38,7 @@ interface DocumentDeleteRequest {
 
 
 const API: AxiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://localhost:9000",
     headers: {"Content-Type": "application/json"},
 });
 
