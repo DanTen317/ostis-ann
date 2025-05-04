4d999eb1ed2c5dcf448a65d6b687786b0d089aa2
DanTen317 <daniil.317.d@gmail.com>
2025-05-04 04:48:20 +0300

    feat: init file for objects

---
+++ problem-solver/py/api/text_processing/objects/__init__.py
@@ -0,0 +1,14 @@
+"""**Object** module contains classes for objects representation"""
+
+from api.text_processing.objects.base import *
+
+__all__ = [
+    'BaseObject',
+    'Block',
+    'TextBlock',
+    'ImageBlock',
+    'TableBlock',
+    'Section',
+    'Chapter',
+    'Document'
+]
